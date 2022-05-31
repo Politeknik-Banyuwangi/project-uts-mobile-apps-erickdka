09d667f617eb6c7801af4ae7c21844237cf91c67
FlutLab.io <>
2022-05-31 05:22:20 +0000

    Autosave

--- lib/chats.dart
+++ lib/chats.dart
@@ -19,7 +19,29 @@ class Chats extends StatelessWidget {
             SizedBox(
               height: 10,
             ),
-            modelchats(),
+            ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage("https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg"),
+                radius: 30.0,
+              ),
+              title: Text(
+                'Anda',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Row(
+                children: [
+                  Icon(
+                    Icons.done_all,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    width: 3.0,
+                  ),
+                  Text('Tugasmu urung mari dap'),
+                ],
+              ),
+              trailing: Text('18.00'),
+            ),
           ],
         ),
       ),
